04946eed0aca66a65f8e0662cda0cb2842647eed
Alex Muscar <alex@muscar.eu>
2021-10-18 20:06:38 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 mod ast_types;
 mod grammar;
 mod graph;
@@ -7,7 +9,11 @@ mod type_gen;
 use crate::{grammar::Grammar, type_gen::TypeGenerator};
 
 fn main() {
-    let g = Grammar::from_file("tests/arithmetic/grammar.json");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("usage: {} <path>", args[0]);
+    }
+    let g = Grammar::from_file(&args[1]);
     let mut ty_gen = TypeGenerator::new();
     for r in g.get_rules() {
         ty_gen.add_rule(&r);
